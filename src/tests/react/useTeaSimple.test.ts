c869f75e97c849b2afac5c6a65857d01121b04a0
dmi <dmi.villione@gmail.com>
2025-05-10 15:51:36 +0300

    wrap happy-path test case into StrictMode

--- src/tests/react/useTeaSimple.test.ts
+++ src/tests/react/useTeaSimple.test.ts
@@ -22,41 +22,42 @@ describe("StrictMode", () => {
         expect(mockHandlers.startLoadingAnimation).toHaveBeenCalledTimes(1);
         expect(result.current[0]).toEqual({ state: 'loading', loadingStarted: 1000 })
     })
-})
 
+    it("happy path", () => {
+        const mockHandlers = {
+            startLoadingAnimation: vi.fn(),
+            displayPopup: vi.fn()
+        }
 
-it("happy path", () => {
-    const mockHandlers = {
-        startLoadingAnimation: vi.fn(),
-        displayPopup: vi.fn()
-    }
+        const { result } = renderHook(() =>
+            useTeaSimple(LoadingState, mockHandlers), { wrapper: StrictMode }
+        )
 
-    const { result } = renderHook(() =>
-        useTeaSimple(LoadingState, mockHandlers)
-    )
+        expect(result.current[0]).toEqual({ state: 'initial' })
+        expect(result.current[1]).toBeDefined()
 
-    expect(result.current[0]).toEqual({ state: 'initial' })
-    expect(result.current[1]).toBeDefined()
-
-    act(() => {
-        result.current[1].started_loading({ now: 1000 })
-    })
-
-    expect(result.current[0]).toEqual({ state: 'loading', loadingStarted: 1000 })
-    expect(mockHandlers.startLoadingAnimation).toHaveBeenCalledTimes(1);
-
-    act(() => {
-        result.current[1].finished_loading({ now: 2000 })
-    })
-
-    expect(result.current[0]).toEqual({
-        state: 'loaded',
-        loadingStarted: 1000,
-        loadingFinished: 2000
-    })
-    expect(mockHandlers.displayPopup).toHaveBeenCalledWith(
-        expect.objectContaining({
-            text: `Loading finished in 1000 milliseconds!`
+        act(() => {
+            result.current[1].started_loading({ now: 1000 })
         })
-    )
+
+        expect(result.current[0]).toEqual({ state: 'loading', loadingStarted: 1000 })
+        expect(mockHandlers.startLoadingAnimation).toHaveBeenCalledTimes(1);
+
+        act(() => {
+            result.current[1].finished_loading({ now: 2000 })
+        })
+
+        expect(result.current[0]).toEqual({
+            state: 'loaded',
+            loadingStarted: 1000,
+            loadingFinished: 2000
+        })
+        expect(mockHandlers.displayPopup).toHaveBeenCalledWith(
+            expect.objectContaining({
+                text: `Loading finished in 1000 milliseconds!`
+            })
+        )
+    })
 })
+
+
